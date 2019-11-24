5d5307fc23f798acdf926b7d13bb58d428ab72f5
John Lekberg <git@johnlekberg.com>
2019-11-23 20:38:29 -0500

    add flag test report

--- pacific-factbook/flag.py
+++ pacific-factbook/flag.py
@@ -9,3 +9,10 @@ def generate() -> str:
             <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
         </svg>
     """
+
+
+if __name__ == "__main__":
+    print("<h1>Test Flag Report</h1>")
+    for _ in range(12):
+        print(generate())
+        print("<hr>")
