7b63fc629176dd158af9a929cc9c3f816b3b06a5
John Lekberg <git@johnlekberg.com>
2019-11-29 17:14:02 -0500

    choose random aspect ratio

--- pacific-factbook/flag.py
+++ pacific-factbook/flag.py
@@ -1,10 +1,17 @@
+from fractions import Fraction
+from random import choice
+
+
 def generate() -> str:
     """
     Generate a random flag. Outputs SVG blob.
 
     """
-    return """
-        <svg width="300" height="200">
+    aspect_ratio = choice([Fraction("2/3"), Fraction("1/2")])
+    height = 200
+    width = height / aspect_ratio
+    return f"""
+        <svg width="{width}" height="{height}">
             <rect width="100%" height="100%" fill="blue"/>
             <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
         </svg>
