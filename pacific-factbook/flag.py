90f072ccc4427cb43d435c01ad4810f069d3a1da
John Lekberg <git@johnlekberg.com>
2019-11-29 17:29:28 -0500

    pick random flag colors

--- pacific-factbook/flag.py
+++ pacific-factbook/flag.py
@@ -7,13 +7,16 @@ def generate() -> str:
     Generate a random flag. Outputs SVG blob.
 
     """
+    colors = set("red blue white green yellow black orange brown gray purple".split())
+    color_background = choice(list(colors))
+    color_shape = choice(list(colors - {color_background}))
     aspect_ratio = choice([Fraction("2/3"), Fraction("1/2")])
     height = 200
     width = height / aspect_ratio
     return f"""
-        <svg width="{width}" height="{height}">
-            <rect width="100%" height="100%" fill="blue"/>
-            <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
+        <svg width="{width}" height="{height}" style="border: 1px solid black">
+            <rect width="100%" height="100%" fill="{color_background}"/>
+            <circle cx="50" cy="50" r="40" stroke="black" stroke-width="4" fill="{color_shape}" />
         </svg>
     """
 
